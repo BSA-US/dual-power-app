d84b6e61f385c5f2f2269bdd4eb3094e4a0cab37
moon <moon@moon.fyi>
2020-03-29 12:45:33 -0400

    ok you can look again

--- api/_gql.ts
+++ api/_gql.ts
@@ -2,21 +2,12 @@ import fetch from 'isomorphic-unfetch'
 
 const { FAUNADB_SECRET: secret } = process.env;
 
-console.log(secret)
-
 interface Props {
   query: String
   variables?: Object
 }
 
 const endpoint = 'https://graphql.fauna.com/graphql'
-
-/*
-const headers = {
-  'Authorization': `Bearer ${secret}`,
-  'Content-Type': 'application/json'
-}
-*/
 
 export default async ({ query, variables }: Props) => {
   const res = await fetch(endpoint, {
@@ -29,12 +20,3 @@ export default async ({ query, variables }: Props) => {
   })
   return await res.json()
 }
-/*
-  !!variables
-    ? await fetch(endpoint, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({ query, variables })
-      })
-    : await fetch(`${ endpoint }?query=${ query }`, { headers })
-    */
