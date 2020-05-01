49a5939f2b3d92c52a52b782c8080c8ca1a7fce1
replygirl <moon@moon.fyi>
2020-05-01 01:07:41 -0400

    feat(seo): #24 add ld-json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,38 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
+import Head from 'next/head'
 import { BaseLayout } from '~/layouts'
 import { Glossary } from '~/components'
 import cn from '~/styles/pages/index.styl'
 
+const meta = {
+  description: 'An open-source platform that provides a framework for organizing toward a worker-owned economy, with tools for founding, funding, governance, and internal + external communications.',
+  imageUrl: 'https://storage.googleapis.com/bsawebsite/dual-power-green.jpg'
+}
+
 const Index: NextPage = () => {
   const [showsGlossary, showGlossary] = useState(false)
 
+  const { description, imageUrl } = meta
+
   return <BaseLayout>
+    <Head>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html:`{
+          "@context": "https://schema.org",
+          "@id": "https://dualpower.app",
+          "@type": "https://schema.org/WebApplication",
+          "description": "${description}",
+          "applicationCategory": "Platform",
+          "applicationSubCategory": "Organizing platform",
+          "countriesSupported": "US",
+          "accessMode": "textual, visual, colorDependent",
+          "accessModeSufficient": "textual, visual",
+          "accessibilitySummary": "No accessibility accommodations have been made yet."
+        }`}}
+      />
+    </Head>
     <main className={cn.index}>
       <article>
         <a onClick={() => showGlossary(false)}><h1>Dual Power App</h1></a>
