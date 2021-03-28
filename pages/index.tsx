abd2e78b3093df61222177037b7064391f817a46
Jarred Harewood <jackhajb@gmail.com>
2021-03-27 20:40:38 -0400

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,73 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
 import { BaseLayout } from '~/layouts'
-import { Glossary } from '~/components'
 import cn from '~/styles/pages/index.styl'
+import { cooperationJacksonUrl, roadmapUrl, twitterUrl, donateUrl } from '~/constants/url.constant';
 
 const Index: NextPage = () => {
-  const [showsGlossary, showGlossary] = useState(false)
+  const donateOnClick = () => { window.location.href = donateUrl }
 
   return <BaseLayout>
     <main className={cn.index}>
+      <div className={cn.donate} onClick={donateOnClick}>
+        <p>Donate</p>
+      </div>
       <article>
-        <a onClick={() => showGlossary(false)}><h1>Dual Power App</h1></a>
-        <h2>Build economic democracy...</h2>
-        <p>
-          The Dual Power App is a platform that provides a framework for organizing toward a worker-owned economy, with tools for founding, funding, governance, and internal + external communications.
-        </p>
-        <p>We’re launching in 2021 with support for Dual Power Projects.</p>
-        <p>
-          <a onClick={() => showGlossary(true)}>What's a Dual Power Project?</a>
-        </p>
+        <header className={cn.header}>
+          <figure>
+          </figure>
+          <div className={cn.social}>
+            <p><a href={twitterUrl}>Twitter</a></p>
+            <p><a href={roadmapUrl}>Roadmap</a></p>
+          </div>
+        </header>
+        <section>
+          <div className={cn.title}>
+            <p>The Dual Power App</p>
+            <p>BUILD ECONOMIC DEMOCRACY</p>
+          </div>
+          <div className={cn.description}>
+            <p>
+              The Dual Power App will be a platform that provides a framework for organizing toward a worker-owned economy, with tools for founding, funding, governance, and internal + external communications.
+            </p>
+            <p>
+              We’re launching in 2021 with support for Dual Power Projects.
+            </p>
+          </div>
+        </section>
+        <section className={cn.power}>
+          <p>WHAT IS A DUAL POWER PROJECT</p>
+          <div>
+            <div>
+              <p>
+                A Dual Power Project is a local, concerted effort to move the economy toward collective ownership and solidarity through the creation of new institutions that provide communities with cooperative control of their labor and the land, as well as housing, health, and banking services.
+              </p>
+            </div>
+            <div>
+              <p>
+                Dual Power Projects are about building a new world in the shell of the old, using existing institutions to create an alternative ecosystem.
+              </p>
+              <p>            
+                Looking for an example? 
+              </p>
+              <p>
+                <a href={cooperationJacksonUrl}>Check out Cooperation Jackson.</a>
+              </p>
+            </div>
+          </div>
+        </section>
+        <div className={cn.break}>
+          <div>
+            <hr/>
+          </div>
+          <div>
+            <hr/>
+          </div>
+        </div>
+        <footer className={cn.footer}>
+          <p>A BSA Open Tech project by Black Socialists in America</p>
+          <p>Coming in 2021</p>
+        </footer>
       </article>
-      { showsGlossary && (<aside><Glossary /></aside>) }
     </main>
   </BaseLayout>
 }
