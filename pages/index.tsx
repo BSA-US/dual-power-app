95637d75b9305f084949bcca9d5b80d4327f4e51
replygirl <moon@moon.fyi>
2021-04-01 19:40:17 -0400

    refactor(components): export launch-page components from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import { BaseLayout } from '~/layouts'
 import cn from '~/styles/pages/index.styl'
-import LaunchPageDonate from '~/components/launch-page/launch-page-donate';
-import LaunchPageHeader from '~/components/launch-page/launch-page-header';
-import LaunchPageTitleSection from '~/components/launch-page/launch-page-title-section';
-import LaunchPagePowerSection from '~/components/launch-page/launch-page-power-section';
-import LaunchPageBreak from '~/components/launch-page/launch-page-break';
-import LaunchPageFooter from '~/components/launch-page/launch-page-footer';
+import {
+  LaunchPageDonate,
+  LaunchPageHeader,
+  LaunchPageTitleSection,
+  LaunchPagePowerSection,
+  LaunchPageBreak,
+  LaunchPageFooter
+} from '~/components/launch-page'
 
 const Index: NextPage = () => {
 
