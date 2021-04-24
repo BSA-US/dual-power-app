52efae35d036c9e340aadcae4434874368914fd1
replygirl <moon@moon.fyi>
2021-04-23 22:02:52 -0400

    remove launch date

--- components/launch-page/launch-page-footer.tsx
+++ components/launch-page/launch-page-footer.tsx
@@ -1,11 +1,9 @@
 import { FC } from 'react';
 import cn from '~/styles/components/launch-page/launch-page-footer.styl'
 
-
 const LaunchPageFooter: FC = () => (
   <footer className={cn.footer}>
     <p>A BSA Open Tech project by Black Socialists in America</p>
-    <p>Coming in 2021</p>
   </footer>
 )
 
