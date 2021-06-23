1b36ae923782b668f466f8b0e455f2e0338c6b3a
replygirl <moon@moon.fyi>
2021-06-23 13:08:56 -0400

    oops

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,4 @@
 export { default as Event } from './event'
-export { default as Modal } from './modal-what'
+export { default as Modal } from './modal'
 export { default as Status } from './status'
 export { default as VideoPlayerStream } from './video-player-stream'
