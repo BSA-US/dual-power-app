708def4f87a2116f9963397f473944ad594a39a7
Scott Trinh <scott@scotttrinh.com>
2022-01-28 15:29:09 -0500

    Add windicss integration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react"
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: config => {
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  },
+  framework: '@storybook/react',
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
+import 'windi.css'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
