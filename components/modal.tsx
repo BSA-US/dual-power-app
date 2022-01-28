0f26bcdc79d196321853088d4df2ab4d2e71e82e
Jarred Harewood <jackhajb@gmail.com>
2022-01-27 19:54:41 -0500

    Feature/modal (#98)
    * feat(component library): add DPA modal using radix dialog
    
    * chore(deps): bump nanoid from 3.1.30 to 3.2.0 (#111)
    
    Bumps [nanoid](https://github.com/ai/nanoid) from 3.1.30 to 3.2.0.
    - [Release notes](https://github.com/ai/nanoid/releases)
    - [Changelog](https://github.com/ai/nanoid/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/ai/nanoid/compare/3.1.30...3.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: nanoid
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * feat(component library): add DPA modal using radix dialog
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Mike Kissinger <mis4mike@gmail.com>

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,35 +1,114 @@
-import type { FC } from 'react'
-import usePortal from 'react-useportal'
+import {
+  Root,
+  Trigger as RadixTrigger,
+  Overlay,
+  Content,
+  Title,
+  Description,
+  Close,
+  DialogPortal,
+  DialogProps,
+  DialogTriggerProps,
+  DialogOverlayProps,
+  DialogContentProps,
+  DialogTitleProps,
+  DialogDescriptionProps,
+  DialogCloseProps,
+  DialogPortalProps,
+} from '@radix-ui/react-dialog'
+import { Cross2Icon } from '@radix-ui/react-icons'
+import { ReactNode } from 'react'
 
-interface ModalProps {
-  classNameClickout?: string
-  classNameContainer?: string
-  isOpen: boolean
-  onRequestClose?: () => void
+interface ModalProps extends DialogProps {
+  className?: string
+  contentClassName?: string
+  trigger?: ReactNode
+  removeCloseIcon?: boolean
+  removeOverlay?: boolean
 }
 
-const ModalComponent: FC<ModalProps> = ({
+const ModalTrigger = ({ className = '', ...rest }: DialogTriggerProps) => (
+  <RadixTrigger
+    asChild
+    className={`font-inter ${className}`.trim()}
+    {...rest}
+  />
+)
+
+const ModalOverlay = ({ className = '', ...rest }: DialogOverlayProps) => (
+  <Overlay
+    className={`fixed -inset-0 bg-black bg-opacity-50 font-inter ${className}`.trim()}
+    {...rest}
+  />
+)
+
+const ModalStandardClose = ({ className = '', ...rest }: DialogCloseProps) => (
+  <Close {...rest} asChild>
+    <button
+      type='button'
+      className={`
+        absolute top-10px right-10px
+        flex justify-center align-middle
+        h-15px w-15px
+        font-inter
+        ${className}
+      `.trim()}
+    >
+      <Cross2Icon />
+    </button>
+  </Close>
+)
+
+const ModalContent = ({ className = '', ...rest }: DialogContentProps) => (
+  <Content
+    className={`
+      fixed inset-center
+      w-90vw max-w-md max-h-85vh
+      p-7 rounded-md
+      bg-white
+      font-inter
+      ${className}
+    `.trim()}
+    {...rest}
+  />
+)
+
+export const ModalTitle = ({ className = '', ...rest }: DialogTitleProps) => (
+  <Title
+    className={`text-lg font-bold mb-2 font-inter ${className}`.trim()}
+    {...rest}
+  />
+)
+
+export const ModalDescription = ({
+  className = '',
+  ...rest
+}: DialogDescriptionProps) => (
+  <Description className={`text-gray-700 ${className}`.trim()} {...rest} />
+)
+
+export const ModalClose = ({ ...rest }: DialogCloseProps) => <Close {...rest} />
+
+export const ModalPortal = ({ ...rest }: DialogPortalProps) => (
+  <DialogPortal {...rest} />
+)
+
+export const Modal = ({
+  trigger,
+  removeCloseIcon,
+  removeOverlay,
+  contentClassName = '',
   children,
-  classNameClickout = '',
-  classNameContainer = '',
-  isOpen,
-  onRequestClose,
-}) => {
-  const { Portal } = usePortal()
-
-  return !isOpen ? null : (
-    <Portal>
-      <div
-        className={`fixed z-9001 inset-center bg-white border p-2 ${classNameContainer}`}
-      >
+  ...rest
+}: ModalProps) => (
+  <Root {...rest}>
+    {trigger && <ModalTrigger>{trigger}</ModalTrigger>}
+    <ModalPortal>
+      {!removeOverlay && <ModalOverlay />}
+      <ModalContent className={contentClassName}>
+        {!removeCloseIcon && <ModalStandardClose />}
         {children}
-      </div>
-      <div
-        className={`fixed z-9000 inset-0 bg-black opacity-10 cursor-pointer ${classNameClickout}`}
-        onClick={() => onRequestClose?.()}
-      />
-    </Portal>
-  )
-}
-
-export default ModalComponent
+      </ModalContent>
+    </ModalPortal>
+  </Root>
+)
