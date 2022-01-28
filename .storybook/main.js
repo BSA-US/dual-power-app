6db729ec310e78efb90bfd29e2d849b87c6800fe
Scott Trinh <scott@scotttrinh.com>
2022-01-28 15:56:59 -0500

    Support `unplugin-icons` bundling

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: config => {
     config.plugins.push(new WindiCSSWebpackPlugin())
+    config.plugins.push(require('unplugin-icons/webpack')())
     config.resolve.modules = [
       ...(config.resolve.modules || []),
       path.resolve(__dirname, '../'),
