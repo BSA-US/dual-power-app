786c6fbbbbf00bc60568aa6e70b0782b4cbe656a
Mike Kissinger <mis4mike@gmail.com>
2022-04-06 18:42:36 -0400

    Feature/yet more simple components (#119)
    * Add divider and card components
    
    * add pill and pill stories
    
    * fix linting errors

---
+++ components/card.tsx
@@ -0,0 +1,34 @@
+export interface CardProps {
+  treatment?: 'standard' | 'knockout' | 'outline'
+  className?: string
+  children: any
+  onClick?: () => void
+}
+
+export const Card = ({
+  treatment = 'standard',
+  className,
+  ...props
+}: CardProps) => {
+  const styleMap = {
+    knockout: 'bg-dark-900 text-white font-bold',
+    outline: 'bg-transparent text-dark-900 font-bold border-3',
+    standard: 'bg-white text-dark-700 border-1 border-stone-200',
+  }
+
+  return (
+    <div
+      className={`p-2 rounded-md m-2 ${styleMap[treatment]} ${className}`}
+      {...props}
+    />
+  )
+}
+
+export const CardCTA = ({ className, ...props }: any) => {
+  return (
+    <div
+      className={`pt-2 flex justify-center ${className}`}
+      {...props}
+    />
+  )
+}
