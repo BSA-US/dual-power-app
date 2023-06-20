a4e06b336d29c76563515228e9fa3f246d41b83d
replygirl <moon@moon.fyi>
2023-06-19 20:49:26 -0500

    tmp: disable custom plugins

--- packages/@replygirl/windicss-plugins/index.ts
+++ packages/@replygirl/windicss-plugins/index.ts
@@ -2,5 +2,5 @@
 // im publishing these under the @replygirl npm scope
 // will replace this directory with npm package when i get to that
 
-export * from './aria'
-export * from './content'
+// export * from './aria'
+// export * from './content'
